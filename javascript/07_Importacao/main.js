3870bb083410f9129cc1fb1f465180116c89cbeb
Inaldo Junior <inaldoanjosjr@gmail.com>
2022-11-29 16:06:42 -0300

    Importação/exportação de arquivos nos projetos

---
+++ javascript/07_Importacao/main.js
@@ -0,0 +1,18 @@
+const funcoes = require('./funcoes_auxiliares');
+/*
+    Uma sala contém 5 alunos e para cada aluno foi sorteado um número de 1 - 100.
+    Faça um programa que receba os 5 números sorteados para os alunos e mostre o
+    maior número sorteado.
+
+    Dados de entrada:
+    5
+    50
+    10
+    98
+    23
+
+    Saída:
+    98
+*/
+
+console.log(funcoes.gets());
